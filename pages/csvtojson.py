98a4cc511b4b0054a5e218a88b981f0adb66ba86
Nomoo0603 <60531777+Nomoo0603@users.noreply.github.com>
2022-02-20 15:53:31 +0800

    nnd

--- pages/csvtojson.py
+++ pages/csvtojson.py
@@ -5,10 +5,11 @@ def make_json(a, b):
     with open(a, encoding = 'utf-8') as csvf:
         csvReader = csv.DictReader(csvf)
         for rows in csvReader:
-            key = rows['No']
+            key = rows['Yes']
             data[key] = rows
     with open(b, 'w', encoding ='utf-8') as jsonf:
         jsonf.write(json.dumps(data, indent = 4))
 a = r"C:/Users/DELL/OneDrive/Documents/GitHub/irts-irts/algorithm/savedata/save.csv"
 b = r"C:/Users/DELL/OneDrive/Documents/GitHub/irts-irts/pages/data.json"
 make_json(a, b)
+
